88371130db3fecc022f1c811bb2defa1e46febb5
Idan <greyayoun@gmail.com>
2025-11-15 19:51:42 +0200

    Add files via upload

---
+++ DecisionLogic/DecisionLogicCode/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+Random dice = new();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+roll1 = 1;
+roll2 = 3;
+roll3 = 2;
+
+Int32 total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total.");
+        total += 6;
+    }
+else
+   
+    Console.WriteLine("You rolled doubles! +2 bonus to total.");
+    total += 2;
+}
+  Console.WriteLine($"Your total including the bonus: {total}");
+
+if (total >= 16)
+{
+    Console.Write("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.Write("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.Write("You win a trip for two!");
+}
+else
+{
+    Console.Write("You win a kitten!");
+}
+
