12e8d0db6ef9a3a1f5230f24ca21e6e1c2da63ee
Idan <greyayoun@gmail.com>
2025-11-15 17:56:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Formats.Asn1;
+
+Random random = new();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+// your code goes here
+
+if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+}
+
+if (daysUntilExpiration == 0)
+{
+    ;Console.Write("Your subscription has expired.");
+}
+
+else if (daysUntilExpiration == 1)
+{
+    Console.Write($"Your subscription expires within a day! Renew now and save {discountPercentage}%.");
+}
+else if (daysUntilExpiration <= 5)
+
+{
+    Console.Write($"Your subscription expires in {daysUntilExpiration} days! Renew now and save {discountPercentage}%.");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.Write("Your subscription will expire soon. Renew now!");
+}
